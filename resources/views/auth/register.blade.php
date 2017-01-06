943cc3cfce02e5c9c6878d53e6bd42914fb3c6ff
jacob <jacob.atam@gmail.com>
2017-01-06 23:36:58 +0100

    changed name to full name in the register controller

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -14,7 +14,7 @@
                             <label for="name" class="col-md-4 control-label">Full Name</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+                                <input id="name" type="text" class="form-control" name="fullname" value="{{ old('name') }}" required autofocus>
 
                                 @if ($errors->has('name'))
                                     <span class="help-block">
