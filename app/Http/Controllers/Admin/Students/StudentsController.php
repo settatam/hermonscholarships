f95ffe1c0529c30d470acce494dd1386db5f3e88
Seth Atam <sethatam@Seths-MBP.fios-router.home>
2017-01-10 00:30:30 -0500

    Testing git

--- app/Http/Controllers/Admin/Students/StudentsController.php
+++ app/Http/Controllers/Admin/Students/StudentsController.php
@@ -12,32 +12,32 @@ use App\Guardian;
 
 
 class StudentsController extends Controller {
-	
-	
-	public function __construct ( ) { 
-	         
-			 
-	   $this->middleware('checkAccess');
 
-	} 
 
-  
-  
+	public function __construct ( ) {
+
+
+	   //$this->middleware('checkAccess');
+
+	}
+
+
+
    public function index()
-    {    
-	
+    {
+
 	    $number_of_students = $this->number_of_students();
-		
+
 		$students = Student::paginate(15);
-		
+
         return view('admin.students.index', compact ('number_of_students','students'));
     }
-	
+
 	 public function create(Request $request)
     {
 		 $number_of_students = $this->number_of_students();
-       if ($request->isMethod('post')) {	
-	       
+       if ($request->isMethod('post')) {
+
 		    //persist to database
 			     $parent                                = new Guardian;
 				 $parent->name                          = $request->name;
@@ -49,13 +49,13 @@ class StudentsController extends Controller {
 				 $parent->state                         = $request->city ;
 				 $parent->job                           = $request->job;
 				 $parent->salary_range 	                = $request->salary;
-				 $parent->save(); 
-				 
+				 $parent->save();
+
 				  $files = $request->file('file');
-				 
+
 				 for ($i = 0; $i < count($request->name); $i++){
-				
-				    $insert = [ 'parent_id'=> $parent->id, 
+
+				    $insert = [ 'parent_id'=> $parent->id,
 					            'user_id'=>\Auth::user()->id,
 								'name'=>$request->student_name[$i],
 								'last_name'=>$request->student_last_name[$i],
@@ -65,40 +65,40 @@ class StudentsController extends Controller {
 								'amount'=>$request->amount[$i]
 					           ];
 		    	    Student::Insert($insert);
-					
+
 				    $files[$i]->move('images/students', $files[$i]->getClientOriginalName());
-				  	  
+
 			   }
-			    
-			    
-				 
+
+
+
 		  return redirect('/admin/students')->with('status', 'Students Details Created');//reject if we have only on address
-	 
-				 	
+
+
 		}
         return view('admin.students.create',compact('number_of_students'));
     }
-	
-		
+
+
 	public  function show(Request $request,$student_id){
-	
+
 	  $student = Student::find($student_id);
-	  
+
 	   $reports = Student::find($student_id)->reports;
-	 
+
 	  $number_of_students = $this->number_of_students();
-	 
+
 	  return view('admin.students.students',compact('number_of_students','student','reports'));
 
-	} 
+	}
 
 	public  function edit (Request $request, $student_id) {
-		
+
 		$student  = Student::find($student_id);
 		$guardian = Guardian::find($student->parent_id);
 		$number_of_students = $this->number_of_students();
-	    if ($request->isMethod('post')) {	
-	       
+	    if ($request->isMethod('post')) {
+
 		         //persist to database
 				 $guardian->name                          = !empty( $request->name ) ? $request->name : $guardian->name;
 				 $guardian->last_name                     = !empty( $request->last_name ) ? $request->last_name : $guardian->name;
@@ -109,56 +109,56 @@ class StudentsController extends Controller {
 				 $guardian->state                         = !empty( $request->state ) ? $request->state :  $guardian->state;
 				 $guardian->job                           = !empty( $request->job ) ?   $request->job  : $guardian->job;
 				 $guardian->salary_range 	              = !empty( $request->salary 	 ) ?   $request->salary : $guardian->salary_range;
-				 $guardian->save(); 
-				 
+				 $guardian->save();
+
 				     $files = $request->file('file');
-				 
-				     if ( $files ) {  
+
+				     if ( $files ) {
 					   	$student->pictures=$files[0]->getClientOriginalName();
 						$files[0]->move('images/students', $files[0]->getClientOriginalName());
-					 } else { 
+					 } else {
 					    $student->pictures=  $request->image_from_database[0];
 
 					 }
-					$student->parent_id =  $guardian->id; 
+					$student->parent_id =  $guardian->id;
 					$student->user_id=\Auth::user()->id;
 					$student->name=$request->student_name[0];
 					$student->last_name=$request->student_last_name[0];
 					$student->description=$request->description[0];
 					$student->grade=$request->grade[0];
 					$student->amount=$request->amount[0];
-					$student->save();        				  	  
-			   
-				 
+					$student->save();
+
+
 		  return redirect()->back()->with('status', 'Students Details Updated');//reject if we have only on address
-	 
-				 	
+
+
 		}
-	   
+
 	    return view('admin.students.create',compact('number_of_students','student','guardian','student_id'));
- 
+
 	}
-	public  function number_of_students ( ) { 
+	public  function number_of_students ( ) {
 	   $count = Student::count();
 	   return count (  $count  )  ? $count : '';
 	}
-	
-	public  function remove ( $student_id) { 
-	
+
+	public  function remove ( $student_id) {
+
 	        $path = base_path().'/public/images/students';
-			
-		
+
+
 		    $images_to_delete = Student::find( $student_id);
-		   
-		   
-		     
+
+
+
 		     \File::Delete($path.'/'.$images_to_delete->pictures);
-			 
-		  
+
+
 	       Student::destroy($student_id);
 		  return redirect()->back()->with('status', 'Students Details rEMOVED ');//reject if we have only on address
 
 	}
-	
-	
+
+
 }
