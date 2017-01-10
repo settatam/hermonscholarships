eb8302c510e79ce483a8caed145148d7484fb79e
Seth Atam <sethatam@Seths-MBP.fios-router.home>
2017-01-10 00:29:09 -0500

    Testing git

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -8,17 +8,17 @@ use App\Http\Controllers\Controller;
 
 use App\Student;
 
-class HomeController extends Controller { 
+class HomeController extends Controller {
 
-     public function __construct ( ) { 
-	         
-			 
-	   $this->middleware('checkAccess');
+     public function __construct ( ) {
 
-	} 
-  
+
+	   //$this->middleware('checkAccess');
+
+	}
+
    public function index()
-    {   
+    {
 	    $number_of_students = Student::count();
         return view('admin.index',compact('number_of_students'));
     }
