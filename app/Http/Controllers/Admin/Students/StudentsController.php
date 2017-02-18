2d08a05cdbffca5d986d92b0319e5f5de988093c
wealthyboy <jacob.atam@gmail.com>
2017-02-18 19:34:17 +0100

    Update StudentsController.php

--- app/Http/Controllers/Admin/Students/StudentsController.php
+++ app/Http/Controllers/Admin/Students/StudentsController.php
@@ -57,7 +57,7 @@ class StudentsController extends Controller {
 				    ]);
 					     
 					 $student->user_id=\Auth::user()->id;
-					 $student->parent_id=\Auth::user()->id;//tempral solution
+					 $student->parent_id=null;//tempral solution
 					 $student->amount=3000;//tempral solution
 					 $student->date_of_birth='today';//tempral solution
 					 $student->name=$request->student_name;
