0a6a51a07ff2aedd96b52702aabca822fc123f67
jacob <jacob.atam@gmail.com>
2017-02-24 08:19:39 +0100

    added the number of users

--- resources/views/layouts/aside.blade.php
+++ resources/views/layouts/aside.blade.php
@@ -3,7 +3,7 @@
            Dashboard
           </a>
           <a href="/admin/students" class="list-group-item">Students  <span class="badge">{{$number_of_students}}</span></a>
-          <a href="/admin/users" class="list-group-item">Users  <span class="badge">14</span></a>
+          <a href="/admin/users" class="list-group-item">Users  <span class="badge">{{$number_of_users}}</span></a>
             <a href="/admin/register" class="list-group-item">Register</a>
 
        </div>
