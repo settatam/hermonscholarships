4478f239dbac1dbb2e53e385a1051f812e2604f3
jacob <jacob.atam@gmail.com>
2017-03-01 13:59:04 +0100

    created freah migrations

---
+++ database/migrations/2017_01_06_212310_create_table_students.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableStudents extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->increments('id');
+			$table->integer('user_id')->unsigned()->nullable();//this refrences the user/sponsors table
+			$table->string('name');
+			$table->string('last_name');
+			$table->text('description');
+			$table->string('grade');
+			$table->string('timeframe')->nullable();
+			$table->timestamp('date_of_birth')->nullable();
+			$table->string('slug')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
