49b49ef1f02235355ed22d60715eda8913f57ebb
Seth Atam <sethatam@Seths-MBP.fios-router.home>
2017-03-13 03:17:15 -0400

    Added Caption

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
 <!-- Main jumbotron for a primary marketing message or call to action -->
         <div class="hero">
             <img class="js-mediaFit" src="/images/banner.jpg" alt="Hermon Scholarships - Help Fund a student" />
-            <div class="caption"> Providing Quality Education to Low income children</div>
+            <div class="hero-caption"> Providing Quality Education to Low income children</div>
         </div>
 
 
