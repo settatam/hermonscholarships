a230fdf891c74d30a050d4d2b5cb292c64149699
Seth Atam <sethatam@Seths-MBP.fios-router.home>
2017-03-18 22:22:38 -0400

    Links updated:

--- resources/views/view/index.blade.php
+++ resources/views/view/index.blade.php
@@ -48,9 +48,9 @@
                <h3 class="">Fill the form below to provide a future for: {{$student->fullname()}}</h3>
                 <hr/>
 
-                <form>
+                <form id="payment-form">
 
-
+                <fieldset>
                 <div class="form-group">
                   <label for="firstname">First Name: <span>*</span></label>
                   <input type="text" class="form-control required" id="firstname" name="firstname">
@@ -73,8 +73,10 @@
                   <input type="text" class="form-control" id="phone" name="phone">
                 </div>
 
+              </fieldset>
 
-              <button class="cta btn btn-primary" type="button">CONTINUE</button>
+
+              <button class="cta btn btn-primary" type="submit">CONTINUE</button>
 
             </form>
 
@@ -98,3 +100,65 @@
      </div>
    </div>
 @endsection
+
+<script>
+
+// Create a Stripe client
+var stripe = Stripe('pk_test_VGtQtFHvb7JYFUwKTAejqKuZ');
+
+// Create an instance of Elements
+var elements = stripe.elements();
+
+// Custom styling can be passed to options when creating an Element.
+// (Note that this demo uses a wider set of styles than the guide below.)
+var style = {
+  base: {
+    color: '#32325d',
+    lineHeight: '24px',
+    fontFamily: 'Helvetica Neue',
+    fontSmoothing: 'antialiased',
+    fontSize: '16px',
+    '::placeholder': {
+      color: '#aab7c4'
+    }
+  },
+  invalid: {
+    color: '#fa755a',
+    iconColor: '#fa755a'
+  }
+};
+
+// Create an instance of the card Element
+var card = elements.create('card', {style: style});
+
+// Add an instance of the card Element into the `card-element` <div>
+card.mount('#card-element');
+
+// Handle real-time validation errors from the card Element.
+card.addEventListener('change', function(event) {
+  const displayError = document.getElementById('card-errors');
+  if (event.error) {
+    displayError.textContent = event.error.message;
+  } else {
+    displayError.textContent = '';
+  }
+});
+
+// Handle form submission
+var form = document.getElementById('payment-form');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  alert('yes');
+
+  stripe.createToken(card).then(function(result) {
+    if (result.error) {
+      // Inform the user if there was an error
+      var errorElement = document.getElementById('card-errors');
+      errorElement.textContent = result.error.message;
+    } else {
+      // Send the token to your server
+      stripeTokenHandler(result.token);
+    }
+  });
+});
+</script>
