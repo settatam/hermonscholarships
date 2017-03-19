723a535459e0568ad0e4cca5b6cf689b4dccf184
Seth Atam <sethatam@Seths-MBP.fios-router.home>
2017-03-19 00:38:03 -0400

    Added Stripe

--- resources/views/view/index.blade.php
+++ resources/views/view/index.blade.php
@@ -162,8 +162,6 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 <script>
 $(function() {
-  alert('loaded');
-
   $('#go-to-form').on('click', function(e) {
       e.preventDefault();
       scrollToDiv('payment-form');
